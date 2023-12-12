e355d9bdc7bf39fc66b62f5f8b13f4b7640c2e1e
Achym <djochymek@hotmail.com>
2023-12-12 19:35:56 +0100

    aa

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Test</Text>
+      <Text>TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
